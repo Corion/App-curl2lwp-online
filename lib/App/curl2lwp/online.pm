10a780c73ab9425663962c926dad941c03719cc6
Max Maischein <corion@corion.net>
2019-10-13 00:03:11 +0200

    Update description

--- lib/App/curl2lwp/online.pm
+++ lib/App/curl2lwp/online.pm
@@ -3,13 +3,17 @@ our $VERSION = '0.02';
 
 =head1 NAME
 
-App::curl2lwp::online - convert curl commands to LWP::UserAgent Perl code online
+App::curl2lwp::online - convert curl commands to Perl code online
 
 =head1 DESCRIPTION
 
 This is the code driving L<https://corion.net/curl2lwp.psgi>. It is a fancy web
 interface to L<HTTP::Request::FromCurl> using L<Mojolicious> for the
 web frontend.
+
+It allows code generation for
+L<LWP::UserAgent> and L<HTTP::Tiny>. While L<HTTP::Tiny> is smaller
+and supports fewer protocols, it comes included with every modern Perl already.
 
 This module is a placeholder to allow installation via C<cpan>.
 
